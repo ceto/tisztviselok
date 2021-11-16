bdf8a9d3e9221e712f567e30fd0d0bc5f599368e
Gabor Szabo <szabogabi@gmail.com>
2021-11-16 23:12:11 +0100

    start

---
+++ src/helpers/svgbar.js
@@ -0,0 +1,27 @@
+const handlebars = require('handlebars');
+module.exports = function(color='', start=25, stop=75) {
+    var hex='';
+    switch (color) {
+        case 'cl1':
+            hex = '#F8A0D5';
+        break;
+        case 'cl2':
+            hex = '#CECBE1';
+        break;
+        case 'cl3':
+            hex = '#F4BDB9';
+        break;
+        case 'cl4':
+            hex = '#FCDC73';
+        break;
+        default:
+          hex = '#666666';
+    }
+    var svg = '<svg class="svgbar" viewBox="0 0 600 15" fill="' + hex +'" width="100%" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">'+
+        '<g mask="url(#barstripes)">' + 
+            '<rect class="base" fill="#efefef" x="0" y="0" width="100%" height="100%" />' +
+            '<rect class="progress" x="'+ start +'%" y="0" width="'+ (stop - start) +'%" height="100%" mask="url(#barmask)" />' +
+        '</g>' +   
+    '</svg>';
+    return new handlebars.SafeString(svg);
+}
