d11a2471c5727ccc53c24f47ea45e2b1481fc0fd
Gabor Szabo <szabogabi@gmail.com>
2021-11-19 15:43:33 +0100

    clen data

--- src/helpers/rightspacecalc.js
+++ src/helpers/rightspacecalc.js
@@ -1,4 +1,6 @@
 const handlebars = require('handlebars');
-module.exports = function(endvalue = 100) {
-    return new handlebars.SafeString(100 - endvalue);
+module.exports = function(dateend = '2033.12.31.') {
+    var hundredpercent = (Date.parse('2033.12.31.') - Date.parse('2010.01.01.'))/1000;
+
+    return new handlebars.SafeString(((Date.parse('2033.12.31.') - Date.parse(dateend))/1000 / hundredpercent * 100));
 }
--- src/helpers/svgbar.js
+++ src/helpers/svgbar.js
@@ -1,6 +1,8 @@
 const handlebars = require('handlebars');
-module.exports = function(color='', start=25, stop=75) {
+module.exports = function(color='', datestart='2010.01.01.', dateend='2033.12.31.') {
     var hex='';
+    var hundredpercent = (Date.parse('2033.12.31.') - Date.parse('2010.01.01.'))/1000;
+
     switch (color) {
         case 'cl1':
             hex = '#CEDED6';
@@ -18,10 +20,12 @@ module.exports = function(color='', start=25, stop=75) {
           hex = '#666666';
     }
     var svg = '<svg class="svgbar" viewBox="0 0 600 15" fill="' + hex +'" width="100%" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">'+
+        '<title>'+ 'Megbízatásának ideje: ' + datestart + ' - ' + dateend + '</title>' +
         '<g mask="url(#barstripes)">' + 
             '<rect class="base" fill="#efefef" x="0" y="0" width="100%" height="100%" />' +
-            '<rect class="progress" x="'+ start +'%" y="0" width="'+ (stop - start) +'%" height="100%" mask="url(#barmask)" />' +
+            '<rect class="progress" x="'+ (100-((Date.parse('2033.12.31.') - Date.parse(datestart))/1000 / hundredpercent * 100)) +'%" y="0" width="'+ ((Date.parse(dateend) - Date.parse(datestart))/1000 / hundredpercent * 100) +'%" height="100%" mask="url(#barmask)" />' +
         '</g>' +   
     '</svg>';
     return new handlebars.SafeString(svg);
 }
+
