bcf1ff2d0975b41b1639cf648e55352f3eb2d464
Gabor Szabo <szabogabi@gmail.com>
2021-11-22 23:58:34 +0100

    waypoint added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'what-input';
 import AOS from 'aos';
 
+
 // Foundation JS relies on a global variable. In ES6, all imports are hoisted
 // to the top of the file so if we used `import` to import Foundation,
 // it would execute earlier than we have assigned the global variable.
@@ -14,9 +15,63 @@ require('foundation-sites');
 // the line below
 // import './lib/foundation-explicit-pieces';
 
+import 'waypoints/lib/noframework.waypoints.js';
+
 
 $(document).foundation();
 
 
 AOS.init();
 
+
+$('.chapter .theaxiswrap').each( function(i,element) {
+
+    var waypointtop = new Waypoint({
+        element: element,
+        handler: function(direction) {
+            if (direction==='down') {
+                $('#globalaxis').addClass('is-shown');
+            } else {
+                $('#globalaxis').removeClass('is-shown');
+            }
+        },
+        // offset:-100
+    })
+
+});
+
+$('.chapter__data').each( function(i,element) {
+    var waypointbtm = new Waypoint({
+        element: element,
+        handler: function(direction) {
+            if (direction==='up') {
+                $('#globalaxis').addClass('is-shown');
+            } else {
+                $('#globalaxis').removeClass('is-shown');
+            }
+        },
+        offset: function() {
+            return -(this.element.clientHeight - 100)
+        }
+    })
+});
+
+let scroll_position = 0;
+let scroll_direction;
+
+window.addEventListener('scroll', function(e){
+    scroll_direction = (document.body.getBoundingClientRect()).top > scroll_position ? 'up' : 'down';
+    scroll_position = (document.body.getBoundingClientRect()).top;
+    if(scroll_direction === 'up'){
+        $('body').addClass('upscrolling').removeClass('downscrolling');
+    } else if(scroll_direction === 'down'){
+        $('body').addClass('downscrolling').removeClass('upscrolling');
+    }
+});
+
+
+    // if(oldValue - newValue < 0){
+    //     $('body').addClass('upscrolling').removeClass('downscrolling');
+    // } else if(oldValue - newValue > 0){
+    //     $('body').addClass('downscrolling').removeClass('upcrolling');
+    // }
