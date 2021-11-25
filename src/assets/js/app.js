c7b559277dd3f2d1de28d11d930cae21ad322dac
Szabó Gábor <szabogabi@gmail.com>
2021-11-25 13:32:04 +0100

    fix + vh test

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -26,14 +26,26 @@ AOS.init({
 });
 
 
-// $('.startgomb').on("click", function(e) {
-//     Foundation.SmoothScroll.scrollToLoc('#start');
-// });
+$('.startgomb').on("click", function(e) {
+    $(this).addClass('dontshow');
+});
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 var leadbasebottom = 0; 
-var afterbannerbasetop = 0
+var afterbannerbasetop = 0;
+var vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 
 function poscheck(){
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
     leadbasebottom = window.innerHeight - ( $('.thelead').offset().top + $('.thelead').height() ); 
     
 }
@@ -48,9 +60,13 @@ $(document).on('scroll', function () {
     // poscheck();
     // console.log($(window).scrollTop() - leadbasebottom + " --- " + leadbasebottom + ' --- ' + window.innerHeight + ' --- ' + ( $(window).scrollTop()) );
     
+
+    $('.startgomb').addClass('dontshow');
+    
     if ( $(window).scrollTop() === 0 ) {
         $('.banner').css('transform', 'translate3d(0, 0, 0)');
         poscheck();
+        $('.startgomb').removeClass('dontshow');
         
     } else if ( ( ($(window).scrollTop() - leadbasebottom ) > 0) && ( ($(window).scrollTop() - leadbasebottom ) <= window.innerHeight) && ( (window.innerHeight + $(window).scrollTop())>0  )) {
         var ytolas = 0 - ($(window).scrollTop() - leadbasebottom );
