19c6216647c35d80d9b5c2c556ea9ff6a04bd9ba
Szabó Gábor <szabogabi@gmail.com>
2021-11-30 13:50:52 +0100

    elesites

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,20 +2,57 @@ import $ from 'jquery';
 import whatInput from 'what-input';
 import AOS from 'aos';
 
+import CC from "CookieConsent";
+// const CC = require( "CookieConsent" );
+
+
+
 
 // Foundation JS relies on a global variable. In ES6, all imports are hoisted
 // to the top of the file so if we used `import` to import Foundation,
 // it would execute earlier than we have assigned the global variable.
 // This is why we have to use CommonJS require() here since it doesn't
 // have the hoisting behavior.
-window.jQuery = $;
-require('foundation-sites');
+// window.jQuery = $;
+// require('foundation-sites');
 
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-// import './lib/foundation-explicit-pieces';
+import './lib/foundation-explicit-pieces';
 
 // import 'waypoints/lib/noframework.waypoints.js';
+
+
+window.cookieconsent.initialise({
+    // container: document.getElementById("cookieConsentModal"),
+    position: "bottom-right",
+    palette:{
+        // popup: {background: "#efefef"},
+        // button: {background: "#aa0000"},
+    },
+    revokable:true,
+    onStatusChange: function(status) {
+     console.log(this.hasConsented() ?
+      'enable cookies' : 'disable cookies');
+    },
+    law: {
+     regionalLaw: false,
+    },
+    location: false,
+    content: {
+        header: 'Cookies used on the website!',
+        message: 'Tájékoztatjuk, hogy a honlapunkon sütiket alkalmazunk. A honlapunk használatával ön a tájékoztatásunkat tudomásul veszi.',
+        dismiss: 'Megértettem',
+        allow: 'Allow cookies',
+        deny: 'Decline',
+        link: 'Bővebben',
+        href: 'https://helsinki.hu/adatkezelesi-tajekoztato/',
+        close: '&#x274c;',
+        policy: 'Cookie Policy',
+        target: '_blank',
+    }
+   });
+
 
 
 $(document).foundation();
@@ -61,3 +98,6 @@ $(document).on('scroll', function () {
     } else if ( $(window).scrollTop() < leadbasebottom ) {
     }  
 });
+
+
+
